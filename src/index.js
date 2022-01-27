12cba221656c57b03d8ae6ded5418e3b87530bbc
Viviane Soares <vianesoaresss@gmail.com>
2022-01-27 03:38:17 -0300

    +1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import validator from './validator.js';
+//* Pegar o nome do titular
+var TitularName = document.getElementById('creditCardName');
+var creditCardNumber = document.getElementById('creditCardNumber');
+var ExpirationDate = document.getElementById('ExpirationDate');
+var CVV = document.getElementById('CardVerificationValue');
 
+
+import validator from './validator.js';
 console.log(validator);
