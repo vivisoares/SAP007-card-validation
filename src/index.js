938ae533e0349979ff7d3256d6da9eed03dbb070
Viviane Soares <vianesoaresss@gmail.com>
2022-02-01 20:12:39 -0300

    Progresso!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
-//* Pegar o nome do titular
-var TitularName = document.getElementById('creditCardName');
-var creditCardNumber = document.getElementById('creditCardNumber');
-var ExpirationDate = document.getElementById('ExpirationDate');
-var CVV = document.getElementById('CardVerificationValue');
-
-
 import validator from './validator.js';
 console.log(validator);
+
+let CCardNumber = document.getElementById('creditCardNumber'); //* Guarda número do cartão 
+let VBttn = document.getElementById('ValidButton'); //** Var. para o botão receber funcionalidade
+
+VBttn.addEventListener("click", Catching); //* Quando o botão for clicado, a função vai ser executada
+
+function Catching() { 
+    //* Função montada
+ //   if (creditCardNumber, creditCardName, ExpirationDate, CardVerificationValue == '' || creditCardNumber, creditCardName, ExpirationDate, CardVerificationValue == null) {
+ //       alert('Por favor, preencha todos os campos!');
+ //   }
+ //   else
+
+
+    let  inputCCNumber = CCardNumber.value; //* Armazenar o valor específico inputado e adicionar em outra váriavel !!!esse valor em específico!!!
+    console.log(inputCCNumber); //* Imprimir o valor digitado
+    console.log(typeof inputCCNumber); //* String
+
+    let arrayCCNumber = inputCCNumber.split(''); //* Valor em array
+    console.log(arrayCCNumber, 'valor em array');
+    console.log(typeof arrayCCNumber);
+
+    let reverseCCNumber = arrayCCNumber.reverse(); //* Valor ao contrário
+    console.log(reverseCCNumber, 'contrário');
+    console.log(typeof reverseCCNumber);
+
+    let pnumCCNumber = parseInt(reverseCCNumber); //* Valor em número, só está pegando o último
+    console.log(pnumCCNumber, 'número');
+    console.log(typeof pnumCCNumber);
+
+
+//for (let i = 0; i < pnumCCNumber.length; i++) {
+
+//     let soma = 0;
+
+//      if (i % 2 === 1) {
+//          valueNumber *= 2;
+//         if (valueNumber > 9) {
+//             valueNumber -= 9;
+//        }
+//        pnumCCnumber[index] = valueNumber;
+ //       console.log(valueNumber, "que?");
+//        soma += valueNumber;
+//  console.log(arrayCCNumber);
+//isValid(inputCCNumber); {
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,57 @@
 const validator = {
-  // ...
-};
+// isValid(inputCCNumber) {
+// let arrayCCNumber = inputCCNumber.split("");
+// console.log(arrayCCNumber, 'aqui o valor em array e number');
 
-export default validator;
+}
+  
+//  let soma = 0; 
+//  for (let i = 0; i < inputCCNumber.length; i++) {
+//       let index = arrayCCNumber.length - i - 1;
+//       let valueNumber = arrayCCNumber[index];
+//
+//       if (i % 2 === 1) {
+//          valueNumber *= 2;
+//          if (valueNumber > 9) {
+//              valueNumber -= 9;
+//          }
+//          arrayCCNumber[index] = valueNumber;
+//       }
+//       console.log(valueNumber, "que?");
+//      soma += valueNumber;
+//  }
+//  console.log(arrayCCNumber);
+
+
+
+//    if (inputCCNumber == "" || inputCCNumber == null || inputCCNumber < 16)
+//      alert ('Digite apenas os números do cartão!');
+
+//    let NumberNumber = Integer.parseInt(inputCCNumber);
+ //   let arrayNumber = []
+//    arrayNumber.push(NumberNumber);
+  
+
+//}
+    //* Looping
+  //  
+    
+
+    //*Looping 2
+//    if (i % 2 != 0) {
+//      inputCCNumber[i] *2
+//    } 
+ //   if (inputCCNumber > 9)
+  //    inputCCNumber -= 0
+  //  }
+ // }
+
+    
+   // const array = array[i]
+
+ //   for (let i = 0; i < array.length; i++);
+ //   ;
+
+ 
+
+export default validator
